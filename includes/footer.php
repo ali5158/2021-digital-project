2f9bd4671b122bb36df4a48a234bdf46f5afa6af
Alex <Ali440tm@gmail.com>
2021-11-06 14:41:48 +1300

    Added the date & time to footer as well as the timezone.

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,11 @@
 </div>
 <link rel="stylesheet" href="css/footer-style.css">
 <div class = "footer">
- <?php echo "<p>Made by Alex Davis in 2021</p>";?>
+ <?php 
+ date_default_timezone_set("pacific/auckland");
+ $currentdate = date("d/m/y h:i A");
+ echo "<p>Made by Alex Davis in 2021 " . $currentdate . "</p>";
+ ?>
 </div>
 </body>
 </html>
